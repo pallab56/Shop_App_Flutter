63833ec3b9f39401ac3afdae1a3ec93914e86d8d
pallab56 <pallab15-5698@diu.edu.bd>
2026-07-14 08:33:43 +0600

    Header Part & Filter part Ui done and font and images added

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,11 +1,98 @@
-
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<String> filtersData = const ['All', 'Adidas', 'Nike', 'Bata'];
+
+  late String selectedFilter;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedFilter = filtersData[0];
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold();
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(color: Color.fromRGBO(225, 225, 225, 1)),
+      borderRadius: BorderRadius.horizontal(left: Radius.circular(50)),
+    );
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    "Shoes\nCollection",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 32,
+                      fontFamily: 'Lato',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: border,
+                      enabledBorder: border,
+                      focusedBorder: border,
+                      prefixIcon: Icon(Icons.search_outlined),
+                      hintText: 'Search',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(
+              height: 50,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: filtersData.length,
+                itemBuilder: (context, index) {
+                  final filters = filtersData[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: GestureDetector(
+                      onTap: () {
+                        selectedFilter = filters;
+                        print('$selectedFilter');
+                        setState(() {
+                          
+                        });
+                      },
+                      child: Chip(
+                        padding: EdgeInsets.all(12),
+                        side: BorderSide(
+                          width: 1,
+                          color:Color.fromRGBO(225, 225, 225, 1),
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        backgroundColor:selectedFilter == filters?Theme.of(context).primaryColor : Color.fromRGBO(245, 247, 249, 1),
+                        label: Text(filtersData[index]),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,25 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromRGBO(254, 240, 1, 1),
+          primary: Color.fromRGBO(254, 240, 1, 1),
+          brightness: Brightness.light
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
+            hintStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold
+            ),
+          )
       ),
-      home:  HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
-
-
-
